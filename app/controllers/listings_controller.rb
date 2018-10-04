6999329d0566f8fd14baaffdd5afe41527482c58
Pio Molina <vpio@comcast.net>
2018-10-03 20:06:23 -0400

    added form for Current user to create new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,16 @@ class ListingsController < ApplicationController
   def index
     @listings = current_user.listings
   end
+
+  def create
+    @description = params[:description]
+    @listing = current_user.listings.new(description: @description)
+    if @listing.save
+      redirect_to listings_path,
+        notice: 'Listing was successfully created.'
+    else
+      redirect_to listings_path,
+        alert: "Could not save listing: #{@listing.errors.full_messages.join(', ')}"
+    end
+  end
 end
