7f9efaadc15b3ca99c28616403c6f5c2413acbd4
Daniel Silver <danielreedsilver@gmail.com>
2018-10-07 13:51:32 -0400

    change listings navbar to my listings and move guests page to listings navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'guests#index'
 
-  resources :listings
+  resources :listings, :guests
   resource :proposals
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
