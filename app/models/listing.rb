81be73f4f876821901dc40ab201c12b10cba5571
Daniel Silver <danielreedsilver@gmail.com>
2018-10-08 16:59:23 -0400

    Add geolocation to form and view page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,12 @@
 class Listing < ApplicationRecord
-  validates :description, presence: true
   belongs_to :user
-
   has_many :proposals
+  validates :description, presence: true
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    [street.upcase, city.capitalize, state.upcase].compact.join(', ')
+  end
+
 end
