542b679b6d5ceae94e06ab910b17dd801e2042c1
Daniel Silver <danielreedsilver@gmail.com>
2018-10-10 15:23:47 -0400

    add jobs page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   resources :listings
   resource :proposals
   resource :map, only: [:show]
+  resource :jobs, only: [:show]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
