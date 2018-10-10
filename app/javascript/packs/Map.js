8d4f859362ac300ab4a5e42342130ec3ae24cbad
Pio Molina <vpio@comcast.net>
2018-10-10 16:48:04 -0400

    added axios and created listings component

--- app/javascript/packs/Map.js
+++ app/javascript/packs/Map.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import Map from '../bundles/Map/components/Map';
+import Listings from '../bundles/Listings/components/Listings';
 
 // This is how react_on_rails can see stuff in the browser.
 ReactOnRails.register({
   Map,
+  Listings,
 });
