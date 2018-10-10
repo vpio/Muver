dd99b529833197d7a50af7f05efd3fcf559560d2
Pio Molina <vpio@comcast.net>
2018-10-10 18:32:50 -0400

    view profile links to miniprofile modal

--- app/javascript/bundles/Listings/components/ListingsModal.jsx
+++ app/javascript/bundles/Listings/components/ListingsModal.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import MiniProfile from './MiniProfile'
 
 class ListingsModal extends React.Component {
   constructor(props) {
      super(props);
      this.state = {
-       modal: false
+       modal: false,
+       user: this.props.user
      };
 
      this.toggle = this.toggle.bind(this);
@@ -18,13 +20,17 @@ class ListingsModal extends React.Component {
    }
 
    render() {
+     const { user } = this.state;
      return (
        <div>
-         <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+         {console.log("hello", user)}
+         <Button color="primary" onClick={this.toggle}>View Profile</Button>
          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
            <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
            <ModalBody>
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+             <MiniProfile
+               user={user}
+               />
            </ModalBody>
            <ModalFooter>
              <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
