6f1a5dcc4d557e2b0dd0862b85981269b748406a
Brendan McCue <BrendanMcCue44@gmail.com>
2018-10-13 17:43:12 -0400

    Added first/last to signup, require first, pointer on map, button styling to delete proposal button

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
 
   has_many :proposals
   has_many :listings
