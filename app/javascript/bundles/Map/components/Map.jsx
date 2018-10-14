43b5227c2af67609498581be07b97271a8bbdafc
Brendan McCue <BrendanMcCue44@gmail.com>
2018-10-14 16:19:58 -0400

    added markers to map

--- app/javascript/bundles/Map/components/Map.jsx
+++ app/javascript/bundles/Map/components/Map.jsx
@@ -1,80 +1,23 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
+import ReactDOMServer from 'react-dom/server'
+import Popup from './Popup'
 
 export default class Map extends Component {
 
-  fetchTasks = () => {
-    const map = this.map;
-    const { lat, lng } = map.getCenter();
-    axios.get(`/map.json?lat=${lat}&lng=${lng}`)
-      .then((response) => { map.getSource('tasks').setData(response.data) })
-      .catch((error) => { console.log(error) });
-  }
-
-
-  createMap = (mapOptions, geolocationOptions) => {
-    this.map = new mapboxgl.Map(mapOptions);
-    const { map } = this;
-    const { lat, lng } = map.getCenter();
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: geolocationOptions,
-        trackUserLocation: true
-      })
-    );
-    var nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, 'bottom-right');
-    map.on('load', (event) => {
-      map.addSource(
-        'listings',
-        {
-          type: 'geojson',
-          data: `/map.json?lat=${lat}&lng=${lng}`
-        }
-      );
-      map.addLayer({
-        id: 'listings',
-        type: 'circle',
-        source: 'listings'
-      })
-      map.on('click', 'listings', (e) => {
-       var coordinates = e.features[0].geometry.coordinates.slice();
-       var description = e.features[0].properties.description;
-       var id = e.features[0].properties.id;
-       while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-           coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-       }
-       new mapboxgl.Popup()
-           .setLngLat(coordinates)
-           .setHTML(`<a href="/listings/${id}">${description}</a>`)
-           .addTo(map);
-       });
-       map.on('mouseenter', 'listings', () => {
-         map.getCanvas().style.cursor = 'pointer';
-       });
-       map.on('mouseleave', 'listings', () => {
-         map.getCanvas().style.cursor = '';
-       });
-       map.on('moveend', () => { this.fetchTasks() });
-    })
-  }
-
-  render(){
-    const style = {
-      width:            '100%',
-      height:           '500px',
-      backgroundColor:  'azure'
+  constructor() {
+    super();
+    this.state = {
+      listings: []
     }
-    return(
-      <div
-        style={style}
-        ref={ el => this.mapContainer = el}
-      />
-    );
   }
 
-  componentDidMount(){
+  async componentDidMount(){
+    // let {data} = await axios.get('/map.json')
+    // console.log('hello world')
+    // console.log(data.features)
+
     mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsc2lsdmVyIiwiYSI6ImNqbjJvaXZ6aTR4b3kzeHFjZ3k2Y2gyY2MifQ.TEKJXzLLLfOj4wJsyHJO1A'
     let { coordinates, centerOnUser } = this.props;
     const mapOptions = {
@@ -93,9 +36,9 @@ export default class Map extends Component {
         // success callback
         (position) => {
           coordinates = [
-                          position.coords.longitude,
-                          position.coords.latitude
-                        ];
+            position.coords.longitude,
+            position.coords.latitude
+          ];
           document.getElementById("long").innerHTML = coordinates[0];
           document.getElementById("lat").innerHTML = coordinates[1];
           mapOptions.center = coordinates;
@@ -110,6 +53,72 @@ export default class Map extends Component {
       this.createMap(mapOptions, geolocationOptions);
     }
   }
+  // fetchTasks = () => {
+  //   const map = this.map;
+  //   const { lat, lng } = map.getCenter();
+  //   axios.get(`/map.json?lat=${lat}&lng=${lng}`)
+  //     .then((response) => { map.getSource('tasks').setData(response.data) })
+  //     .catch((error) => { console.log(error) });
+  // }
+
+
+  createMap = (mapOptions, geolocationOptions) => {
+    this.map = new mapboxgl.Map(mapOptions);
+    const { map } = this;
+    const { lat, lng } = map.getCenter();
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: geolocationOptions,
+        trackUserLocation: true
+      })
+    );
+
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'bottom-right');
+
+    map.on('load', (event) => {
+      this.fetchPlaces();
+      map.on('moveend', (e) => {
+        this.fetchPlaces();
+      })
+    })
+  }
+
+  async fetchPlaces() {
+    const map = this.map;
+    const self = this;
+    const { lat, lng } = map.getCenter();
+    let {data} = await axios.get(`/map.json`)
+    data = data.features
+    data.forEach((listing, i) => {
+      let elm = document.createElement('div')
+      elm.className = "mapbox-marker"
+      let popup = new mapboxgl.Popup({ offset: 25 })
+      .setHTML(
+        `<a>${listing.properties.description}</a> `
+      )
+      let marker = new mapboxgl.Marker(elm)
+      .setLngLat(listing.geometry.coordinates)
+      .setPopup(popup)
+      marker.addTo(map)
+    })
+  }
+
+  render(){
+    const style = {
+      width:            '100%',
+      height:           '500px',
+      backgroundColor:  'azure'
+    }
+    return(
+      <div
+        style={style}
+        ref={ el => this.mapContainer = el}
+      />
+    );
+  }
+
 
   componentWillUnmount() {
     this.map.remove();
