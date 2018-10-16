06077bede0abaa3e32bb23e1beaf783fedba2b59
Pio Molina <vpio@comcast.net>
2018-10-15 20:22:06 -0400

    added response page for contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,4 +9,14 @@ class StaticPagesController < ApplicationController
     @id = current_user.listings.last.id
   end
 
+  def response_page
+    user = current_user || ""
+    @user =
+    unless user.is_a?(String)
+      current_user.first_name
+    else
+      user
+    end
+  end
+
 end
